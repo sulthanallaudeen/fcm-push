46acea709429122e41879f295e8e2e8508ca1b7b
sulthanallaudeen <sa@sulthanallaudeen.com>
2017-08-28 00:59:08 +0530

    Custom js modified to get FCM and Key from View

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $("#sendPush").click(function() {
         var url = window.location.href + '/';
-        var key = "AAAAalLjbyM:APA91bE9ZeLmlJ6i5DaTo6-12q_7YM1eT2HYQAl4XbpAmGchFkBmLEvarNkM9Jmcdk-kaZeCqEW-tVfGqK7OGTuJJdKyfsNgGGdPrgCJlwPVLG4BW21vGN527VBMq9yJntlGZCV5YvXu";
-        var to = 'cisKTNAQ1Zk:APA91bE2tRQ7z1ClY27fbrkEdYnaN7T-OW6vfCkJd2zhdRM3GoKxMqAtGLqzCOAIWHAFo6DSDeL-m-jwYKCo2KmlhYipiKHY-pdo8xMYDmhh9qnJKmDf6JN590GAJ6ONUFtQ29yW96o6';
+        var key = $("#key").val();
+        var to = $("#fcm_token").val();
         var title = $("#title").val();
         var body = $("#body").val();
         $.post("sendPush", { key: key, to: to, title: title, body: body })
